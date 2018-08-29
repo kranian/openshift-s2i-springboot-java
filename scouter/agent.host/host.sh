697a3ce5b458fa5f416e5c7785b9875bc345e77b
ysheo <ysheo@irexnet.co.kr>
2018-08-29 17:48:29 +0900

    Update pinpoint -> scouter

--- scouter/agent.host/host.sh
+++ scouter/agent.host/host.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-exec java  -classpath /opt/app-root/scouter/agent.host/scouter.host.jar \
-            scouter.boot.Boot \
-            /opt/app-root/scouter/agent.host/lib > nohup.out &
+exec java  -classpath .:/opt/app-root/scouter/agent.host/scouter.host.jar \
+           -Dscouter.config=/opt/app-root/scouter/agent.host/conf/scouter.conf \
+           scouter.boot.Boot \
+           /opt/app-root/scouter/agent.host/lib > nohup.out &
 
